6b5a03ede4b82d5d7ed0900df465ff56ec61190b
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-08-18 13:20:02 +0200

    feat : Add robots and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+const siteUrl = "https://parisa-portfolio-nu.vercel.app";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: siteUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
